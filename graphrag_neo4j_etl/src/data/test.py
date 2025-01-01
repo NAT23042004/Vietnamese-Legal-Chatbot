13ba5a0221ea642ba29a17fdb3af75ab3ba777d6
NAT23042004 <dragonslayer23042004@gmail.com>
2025-01-01 17:52:53 +0700

    Remove env file and graphrag_neo4j_etl

--- graphrag_neo4j_etl/src/data/test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import sys
-
-graph = pd.read_csv("graph-export.csv")
-node = pd.read_csv("node-export.csv")
-relationship = pd.read_csv("relationship-export.csv")
-
-print(graph['noidung'])
-print(node['name'].head().info)
--- graphrag_neo4j_etl/src/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run any setup steps or pre-processing tasks here
-echo "Running ETL to move legal data from docs file to Neo4j..."
-
-# Run the ETL script
-python kg_constructing.py
